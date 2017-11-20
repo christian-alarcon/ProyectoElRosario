b183155872ddb9b83f1a6e1f66823e3135d6d072
Diego <Diego@Admin>
2017-11-20 13:20:08 -0500

    comentario prueba 2

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -17,6 +17,7 @@ public class Proyecto {
     public static void main(String[] args) {
         // TODO code application logic here
         //comentario
+        //comentario2
     }
     
     public void cargarUsuarios(){
